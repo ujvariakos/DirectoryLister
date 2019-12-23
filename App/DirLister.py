a225199d67c59993c4eeb7101b9ec5080af05ed0
ujvariakos <ujvari.akos4@gmail.com>
2019-12-23 10:18:46 +0100

    Feature
    Script can print full path

--- App/DirLister.py
+++ App/DirLister.py
@@ -1,1 +1,26 @@
-print("hellooo")
+import os
+
+class DirLister:
+
+    def __init__(self,):
+        pass
+
+    def showDir(self, dir: str):
+        path = os.path.normpath(dir)
+        print(r'--', sep=' ', end='', flush=True)
+        if os.path.isdir(path):
+
+            print(path)
+            dirs = os.listdir(path)
+            for directory in dirs:
+                new_path = os.path.join(path, directory)
+                print(r'--', sep=' ', end='', flush=True)
+                self.showDir(str(new_path))
+        elif os.path.isfile(path):
+            print(r'-- ', sep=' ', end='', flush=True)
+            print(path)
+        else:
+            raise ValueError("Invalid directory")
+
+dir_lister = DirLister()
+dir_lister.showDir(r'd:/PythonProjects/test0/')
